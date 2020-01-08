f883c5e5c9a9754ec3e8a87080f3d8ea9328c27f
LislPisl <eroesch@student.unimelb.edu.au>
2020-01-08 12:55:24 +1100

    col yes!

--- paper/vdP/van_der_pol_coll.jl
+++ paper/vdP/van_der_pol_coll.jl
@@ -1,16 +1,19 @@
-
 using  Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, OrdinaryDiffEq
 using BSON: @save
 u0 = Float32[2.; 0.]
 datasize = 200
-tspan = (0.0f0, 14.f0)
+tspan = (0.0f0, 7.f0)
 t = range(tspan[1], tspan[2], length = datasize)
 function trueODEfunc(du, u, p, t)
   du[1] = u[2]
   du[2] = (1-u[1]^2)*u[2]-u[1]
   return du
 end
-
+est_c = "#D6B656"
+pred_col_c = "#82B366"
+pred_l2_c = "#9673A6"
+obs_c = "#6C8EBF"
+col=pred_col_c
 prob = ODEProblem(trueODEfunc, u0, tspan)
 ode_data = Array(solve(prob,Tsit5(),saveat=t))
 species1 = "Van der Pol oscillator"
@@ -27,7 +30,6 @@ scatter!(t, ode_data[2,:], label="", color ="blue")
 plot!(t, ode_data[1,:], label="", color ="red")
 plot!(t, ode_data[2,:], label="", color ="blue")
 #savefig("paper/vdP/Obs_time.pdf")
-
 
 mutable struct saver
     losses::Array{Float64,1}
@@ -69,7 +71,7 @@ n_ode = x->neural_ode(dudt, x, tspan, Tsit5(), saveat=t, reltol=1e-7, abstol=1e-
 n_epochs = 3501
 verify = 50 # for <verify>th epoch the L2 is calculated
 data1 = Iterators.repeated((), n_epochs)
-opt1 = Descent(0.00001)
+opt1 = Descent(0.0001)
 sa = saver(n_epochs)
 L2_loss_fct() = sum(abs2,ode_data .- n_ode(u0))
 # Callback function to observe two stage training.
@@ -145,10 +147,7 @@ scatter!(t,Flux.data(pred[test[1],:]), label="", color ="green", grid = "off",fr
 scatter!(t, Flux.data(pred[test[2],:]), label="", color ="brown")
 plot!(t,Flux.data(pred[test[1],:]), label="", color ="green")
 plot!(t, Flux.data(pred[test[2],:]), label="", color ="brown")
-est_c = "#D6B656"
-pred_col_c = "#82B366"
-pred_l2_c = "#9673A6"
-obs_c = "#6C8EBF"
+
 esti =loss_n_ode.estimated_solution
 
 
