df20d085958c149be0c78c4d7db0eb10b1facffe
LislPisl <eroesch@student.unimelb.edu.au>
2020-01-12 19:14:29 +1100

    figures done ?

--- paper/vdP/load_and_plot_loss_drop.jl
+++ paper/vdP/load_and_plot_loss_drop.jl
@@ -9,7 +9,6 @@ foldername3 = "col_high_noise"
 pred_col_c = "#82B366"
 pred_col_2 = "#800080"
 
-
 l2s = JLD.load(string("paper/vdP/",foldername,"/savel2s_re.jld"))["l2s"]
 losses = JLD.load(string("paper/vdP/",foldername,"/savelosses_re.jld"))["losses"]
 times = JLD.load(string("paper/vdP/",foldername,"/savetimes_re.jld"))["times"]
@@ -27,8 +26,9 @@ losses3 = JLD.load(string("paper/vdP/",  foldername3, "/savelosses_re.jld"))["lo
 times3 = JLD.load(string("paper/vdP/", foldername3, "/savetimes_re.jld"))["times"]
 
 n = length(l2s)
+n=4400
 step_size = n/5
-selection = range(1,step = 50, stop =n)
+selection = range(1,step = 2, stop =n)
 selection_snips = Array(range(500,step = step_size, stop =n))
 #pl_1_x=range(1,stop=length(sa_l2.losses))[selection]
 #pl_1_y=log.(sa_l2.losses)[selection]
@@ -44,18 +44,19 @@ pl_2_y2 = log.(losses2)[selection]
 pl_2_x3 = range(1,stop=length(losses3))[selection]
 pl_2_y3 = log.(losses3)[selection]
 
-alphas = [1.,0.6,0.4,0.2]
-alphas = [1.,1.,6.,3.]
 
+alphas = [1.,.3,.6,1.]
+# grey and red
+cols = ["#696969","#920005","#920005","#920005"]
 
 #map(x -> 1/x, pl_2_y3)
 #plot(pl_1_x,pl_1_y, color = pred_l2_c, margin=5Plots.mm, width =2, label =labels[1],  grid = "off")
 #scatter!([pl_1_x[1],pl_1_x[end]],[pl_1_y[1],pl_1_y[end]], color = pred_l2_c, margin=5Plots.mm, width  =2, label ="",  grid = "off")
-a =plot(pl_2_x3, pl_2_y3, width = 2, color = pred_col_c, alpha = alphas[4],label = labels[4],
-                xlab = "Training epoch", ylab = "Log(Loss)", legend = :bottomleft, grid = "off")
-plot!(pl_2_x2, pl_2_y2, width = 2, color = pred_col_2,alpha = alphas[3], label = labels[3],  grid = "off")
-plot!(pl_2_x1, pl_2_y1, width = 2, color = pred_col_2, alpha = alphas[2], label = labels[2], grid = "off")
-plot!(pl_2_x, pl_2_y, width = 2, color = pred_col_2, alpha = alphas[1],label = labels[1])
+a = plot(pl_2_x, pl_2_y, width = 2.5, color = cols[1], alpha = alphas[1],label = labels[1])
+plot!(pl_2_x1, pl_2_y1, width = 2.5, color = cols[2], alpha = alphas[2], label = labels[2], grid = "off")
+plot!(pl_2_x2, pl_2_y2, width = 2.5, color = cols[3],alpha = alphas[3], label = labels[3],  grid = "off")
+plot!(pl_2_x3, pl_2_y3, width = 2.5, color = cols[4], alpha = alphas[4],label = labels[4],
+                xlab = "Training epoch", ylab = "Log(Loss)", margin=5Plots.mm, legend = :bottomleft, grid = "off")
 
 #scatter!([ pl_2_x[end]], [pl_2_y[end]], color = pred_col_c, width  = 2, label = "")
 #scatter!([pl_2_x1[end]], [pl_2_y1[end]], color = pred_col_2, markershape = :utriangle, width = 2, label = "")
@@ -66,10 +67,3 @@ plot!(pl_2_x, pl_2_y, width = 2, color = pred_col_2, alpha = alphas[1],label = l
 
 
 savefig(string("paper/vdP/noise_figure_selected_sub_plots/loss_drop_noise_levels.pdf"))
-
-
-
-
-a = [1,2,3]
-
-(a)
--- paper/vdP/load_and_plot_time_courses.jl
+++ paper/vdP/load_and_plot_time_courses.jl
@@ -1,10 +1,19 @@
 using Flux, DiffEqFlux, OrdinaryDiffEq, DiffEqParamEstim, Plots, Dates
 using BSON: @load
+# green
 pred_col_c = "#82B366"
-pred_l2_c = "#C698DB"
+# lila blass
+#pred_l2_c = "#C698DB"
+#grey
 obs_c = "#696969"
-ref_c = "#920005"
-# nice red: "#920005"
+# grey
+ref_c = "#C698DB"
+# grey and red
+cols = ["#696969","#920005","#920005","#920005"]
+#markerlinewidh low or line off for obs schatter
+col = pred_l2_c
+col = pred_col_c
+alphas = [1.,.1,.1,1.]
 u0 = Float32[2.; 0.]
 datasize = 200
 end_train = 7.
@@ -24,45 +33,48 @@ function trueODEfunc(du, u, p, t)
 end
 
 
-
+n_epochs=4401
 key_list = Array(range(1,step=50,stop=n_epochs))
-key_list = [3501]
-lime = maximum(ode_data)
-col = pred_l2_c
-col = pred_col_c
+key_list = [4401]
 
-foldernames = [ "col", "col_low_noise", "col_medium_noise", "col_high_noise"]
+
+
+foldernames = [ "col_no_noise", "col_low_noise", "col_medium_noise", "col_high_noise"]
+
 noises = [0.0,0.1,0.2,0.5]
+
 #Plots.scalefontsizes(0.8)
 for i in 1:length(foldernames)
         foldername = foldernames[i]
         noise = noises[i]
-    key_t = "3501"
-    @load string("paper/vdP/", foldername, "/", key_t, "te_dudt.bson") dudt
+    key_t = "4401"
+
+    @load string("paper/vdP/", foldername, "/", key_t, "te_dudt_re.bson") dudt
     n_ode = x->neural_ode(dudt, x, tspan_test, Tsit5(), saveat=t_test, reltol=1e-7, abstol=1e-9)
     pred = n_ode(u0)
     prob = ODEProblem(trueODEfunc, u0, tspan_train)
     prob_ref = ODEProblem(trueODEfunc, u0, tspan_test)
 
     ode_data = Array(solve(prob,Tsit5(),saveat=t_train))
+    lime = maximum(ode_data)
     noise = rand(size(ode_data)[1],size(ode_data)[2]).*noise
     ode_data = ode_data.+noise
     ode_data_ref = Array(solve(prob_ref,Tsit5(),saveat=t_test))
-    a=  scatter(t_train, ode_data[1,:], label = "", color = obs_c, grid = "off",framestyle = :box,
-            ylim = (-3.5,3.5), size = (500,100), markersize = 2,
+    a=  scatter(t_train, ode_data[1,:], label = "", color = cols[i], alpha = alphas[i], markerstrokewidth=0.001, grid = "off",framestyle = :box,
+            ylim = (-3.5,3.5), size = (500,100), markersize = 2.35,
             yticks= ([-lime,0,lime],["","",""]),
             xticks= ([0,end_train, end_test],["","",""]))
-    scatter!(t_train, ode_data[2,:], label="", color =obs_c, markersize = 2)
+    scatter!(t_train, ode_data[2,:], label="", color = cols[i], alpha = alphas[i], markerstrokewidth=0.001, markersize = 2.35)
     #plot!(t_train, ode_data[1,:], label="", color =obs_c)
     #plot!(t_train, ode_data[2,:], label="", color =obs_c)
     #scatter!(t_test,Flux.data(pred[test[1],:]), label="", color =col, grid = "off",framestyle = :box)
     #scatter!(t_test, Flux.data(pred[test[2],:]), label="", color =col)
-    plot!(t_test,Flux.data(pred[test[1],:]), linewidth = 2, label = "", color = col)
-    plot!(t_test, Flux.data(pred[test[2],:]),linewidth = 2, label = "", color = col)
-    plot!(t_test[200:end], ode_data_ref[1,:][200:end], linestyle = :dash, label = "", linewidth = 2, color = ref_c)
-    plot!(t_test[200:end], ode_data_ref[2,:][200:end], linestyle = :dash, label = "", linewidth = 2, color = ref_c)
+    plot!(t_test,Flux.data(pred[1,:]), linewidth = 2, label = "", color = pred_col_c)
+    plot!(t_test, Flux.data(pred[2,:]),linewidth = 2, label = "", color = pred_col_c)
+    plot!(t_test[200:end], ode_data_ref[1,:][200:end], linestyle = :dash, label = "", linewidth = 2, color = obs_c)
+    plot!(t_test[200:end], ode_data_ref[2,:][200:end], linestyle = :dash, label = "", linewidth = 2, color = obs_c)
 
     display(a)
     #savefig(string("paper/vdP/", foldername, "/plots/time_course_", key_t, "te_fit_selected_testing.pdf"))
-    savefig(string("paper/vdP/noise_figure_selected_sub_plots/", foldername,"reruns.pdf"))
+    savefig(string("paper/vdP/noise_figure_selected_sub_plots/", foldername,"reruns_new.pdf"))
 end
